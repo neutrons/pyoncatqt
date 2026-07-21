00e0264d79bb57011ca990767d80e81335429314
Daniel Caballero <dlcaballero16@gmail.com>
2026-07-21 13:33:38 -0400

    Fix build conda package issue

---
+++ src/pyoncatqt/__init__.py
@@ -0,0 +1,6 @@
+"""pyoncatqt: common GUI elements for ONCat."""
+
+try:
+    from pyoncatqt._version import __version__  # noqa: F401
+except (ModuleNotFoundError, ImportError):
+    __version__ = "0.0.1"
